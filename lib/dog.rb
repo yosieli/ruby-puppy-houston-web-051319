0bd03a2ce63d3c0ef24ea9bfa5c04dde87c02370
feven <feventsegay6@gmail.com>
2019-05-15 04:07:18 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  @@all = []
+  attr_accessor :name
+  def initialize(name)
+    @name=name
+  @@all << self
+  end
+  def self.all
+    @@all.each do |dog|
+     puts  dog.name
+  end
+  end
+  def self.clear_all 
+    @@all.clear
+  end
+end
